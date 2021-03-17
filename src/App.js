e7930a33159e98b036b58553ad5fa122578bd9a8
Bhargavamacha <mbsai29@gmail.com>
2021-03-17 09:45:20 +0530

    added tech

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* es-lint disable */
 import React, { useState } from 'react';
 import './App.css';
 import Grid from '@material-ui/core/Grid';
@@ -7,12 +8,13 @@ import Skills from './components/Skills'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
 import Certifications from './components/Certifications'
+import Tech from './components/Tech'
 import Contact from './components/Contact'
 import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 function App() {
   const contact = React.createRef()
   const scrollToBottom = () => {
-    contact.current.scrollIntoView({ behavior: 'smooth'})
+    contact.current.scrollIntoView({ behavior: 'smooth' })
   }
   const theme = createMuiTheme({
     typography: {
@@ -23,21 +25,21 @@ function App() {
   })
   const [select, setselect] = useState('none')
   return (
-    <div className="App" style={{overflow:'hidden'}}>
+    <div className="App" style={{ overflow: 'hidden' }}>
       <ThemeProvider theme={theme}>
         <Grid container spacing={4}>
           <Grid item xs={12} sm={5} >
             <div style={{ marginLeft: 50, marginTop: 40, position: window.innerWidth > 600 ? 'fixed' : 'relative' }}>
               <Grid item xs={12} sm={8} >
-                <img src={require('./assets/me.jpg')} alt={'Couldn\'t load'} style={{ width: 150, height: 150, borderRadius: 400 }} />
+                <img src='https://storageapi.fleek.co/bhargavamacha-team-bucket/me.jpg' alt={'Couldn\'t load'} style={{ width: 150, height: 150, borderRadius: 400 }} />
                 <p style={{ fontSize: 23 }}>Hey there!<br />
                   <p style={{ display: 'inline', backgroundColor: 'black', color: 'white' }}>This is Bhargava.</p><br />
-            Pronouns: He/Him/His
-            </p>
-                <p style={{ fontSize: 20 }}>Junior @ IIT Kharagpur.<br />	&#10084; Everything Tech, Everything Pop.<br /> Making tech work while catching up with Netflix.</p>
+                  Pronouns: He/Him/His
+                </p>
+                <p style={{ fontSize: 20}}>Junior @ IIT Kharagpur.<br />	Learning and working with technologies that <u>will</u> <br />power the next couple of decades... or maybe more?<br /> </p>
                 {/* <Button onClick={scrollToBottom} style={{ backgroundColor: 'black', marginRight: 20, fontFamily: 'Source Code Pro', color: 'white' }}>Contact</Button> */}
-                <Button onClick={()=> window.open("https://resume.bhargavamacha.info", "_blank")} style={{ backgroundColor: 'black', color: 'white', fontFamily: 'Source Code Pro' }}>RESUME</Button>
-                <p style={{ fontSize: 22 }}>Let's get connected!</p>
+                <Button onClick={() => window.open("https://resume.bhargavamacha.info", "_blank")} style={{ backgroundColor: 'black', color: 'white', fontFamily: 'Source Code Pro' }}>RESUME</Button>
+                <p style={{ fontSize: 22 }}>Connect with me @</p>
                 <SocialIcon onMouseEnter={() => setselect('twitter')} onMouseLeave={() => setselect('none')} url="http://twitter.com/machabhargava" style={{ marginRight: 15, height: 35, width: 35, opacity: select === 'twitter' ? 1 : select === 'none' ? 1 : 0.2 }} />
                 <SocialIcon onMouseEnter={() => setselect('github')} onMouseLeave={() => setselect('none')} url="https://github.com/Bhargavamacha" style={{ marginRight: 15, height: 35, width: 35, opacity: select === 'github' ? 1 : select === 'none' ? 1 : 0.2 }} />
                 <SocialIcon onMouseEnter={() => setselect('insta')} onMouseLeave={() => setselect('none')} url="https://www.instagram.com/bhargava_macha/" style={{ marginRight: 15, height: 35, width: 35, opacity: select === 'insta' ? 1 : select === 'none' ? 1 : 0.2 }} />
@@ -48,10 +50,17 @@ function App() {
             </div>
           </Grid>
           <Grid item xs={12} sm={7}>
+          <div style={{ backgroundColor: '#aec9eb', color: 'black' }}>
+              <div style={{ marginLeft: 40 }}>
+                <br />
+                <p style={{ marginTop: 0, fontSize: 30 }}>Tech I am currently working on</p>
+                <Tech />
+              </div>
+            </div>
             <div style={{ backgroundColor: 'lightgreen', color: 'black' }}>
               <div style={{ marginLeft: 40 }}>
                 <br />
-                <p style={{ marginTop: 0, fontSize: 30 }}>SKILLS</p>
+                <p style={{ marginTop: 0, fontSize: 30 }}>Skills</p>
                 <Skills />
               </div>
             </div>
@@ -65,7 +74,7 @@ function App() {
             <div style={{ backgroundColor: 'lightblue', color: 'black', marginTop: -40, }}>
               <div style={{ marginLeft: 40 }}>
                 <br />
-                <p style={{ marginTop: 0, fontSize: 30 }}>PROJECTS</p>
+                <p style={{ marginTop: 0, fontSize: 30 }}>Projects</p>
                 <Projects />
                 <br />
                 <br />
@@ -74,7 +83,7 @@ function App() {
             <div style={{ backgroundColor: '#fed8b1', color: 'black', marginBottom: -20 }}>
               <div style={{ marginLeft: 40 }}>
                 <br />
-                <p style={{ marginTop: 0, fontSize: 30 }}>CERTIFICATIONS</p>
+                <p style={{ marginTop: 0, fontSize: 30 }}>Certifications</p>
                 <Certifications />
                 <br />
                 <br />
---
+++ src/components/Tech.js
@@ -0,0 +1,14 @@
+/* es-lint disable */
+import React from 'react';
+export default function Tech() {
+    return (
+        <div style={{ lineHeight:2}}>
+            1. Ethereum (Mostly Dapps)<br />
+            2. Ethereum Alternatives (Gas price!!!!)<br />
+            2. AR/VR<br />
+            3. Web3.0 - IPFS/Filecoin/DFINITY/Storj (This website is hosted via fleek on IPFS!)<br />
+            4. Pen Testing<br />
+            5. App dev, web dev and cloud deployments<br /><br />
+        </div>
+    );
+}
